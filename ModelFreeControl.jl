06023aa2017690f92e4c97ade33cb18d22e8bc46
hataloo <jacksandberg998@gmail.com>
2022-06-11 20:50:01 +0200

    Updated alpha decay in Qlearning, added statistics to MFC

--- ModelFreeControl.jl
+++ ModelFreeControl.jl
@@ -3,13 +3,36 @@ include("TabularRL.jl")
 
 hallwayMDP = getHallwayMDP(10, 0.9)
 
+V_vi = ValueIteration(hallwayMDP, 1000)
+Q_vi = ActionValueIteration(hallwayMDP, 1000)
 
-Q = ActionValueIteration(hallwayMDP, 1000)
-
-π_ϵ = EpsilonGreedyPolicy(hallwayMDP, 1.0, 0.05, Int64(2e6))
+π_ϵ = EpsilonGreedyPolicy(hallwayMDP, 1.0, 0.01, Int64(2e6))
 π_ϵ.ϵ_current_iteration
 sample(π_ϵ, 10, Q)
 
-Q_sarsa = SARSA(π_ϵ, hallwayMDP, 3000, 1000)
-Q_qlearning = QLearning(π_ϵ, hallwayMDP, 3000, 1000)
-Q_double1, Q_double2 = DoubleQLearning(π_ϵ, hallwayMDP, 1000, 2000)
+N_episodes = 200000
+T_max = 100
+Q_sarsa = SARSA(π_ϵ, hallwayMDP, N_episodes, T_max)
+Q_qlearning = QLearning(π_ϵ, hallwayMDP, N_episodes, T_max)
+Q_double1, Q_double2 = DoubleQLearning(π_ϵ, hallwayMDP, N_episodes, T_max)
+
+Q_results = OrderedDict{String, Any}([
+    ("sarsa", Q_sarsa), 
+    ("Q-learning", Q_qlearning), 
+    ("DoubleQ", Q_double1)])
+Q_resultsSummary = OrderedDict{String, Any}()
+for (k,Q_res) in Q_results
+    Qdeviation = abs.(Q_res - Q_vi)
+    Vdeviation = abs.(Q_res[argmax(Q_res, dims = 2)] - V_vi)
+    PolicyDeviation = argmax(Q_res, dims = 2) .!= argmax(Q_vi, dims = 2)
+    Q_resultsSummary[k] = OrderedDict([
+        ("meanQDev", mean(Qdeviation)),
+        ("minQDev", minimum(Qdeviation)),
+        ("maxQDev", maximum(Qdeviation)),
+        ("meanVDev", mean(Qdeviation)),
+        ("minVDev", minimum(Qdeviation)),
+        ("maxVDev", maximum(Qdeviation)),
+        ("meanPolicyDeviation", mean(PolicyDeviation)),
+    ])
+end
+Q_resultsSummary
--- QLearning.jl
+++ QLearning.jl
@@ -1,28 +1,32 @@
-function QLearning(π::GLIEPolicy, mdp::MDP, N_episodes::Number, T::Number)
+function QLearning(π::GLIEPolicy, mdp::MDP, N_episodes::Number, T::Number, α = nothing)
     reset!(π)
+    if α === nothing
+        α = LinRange(1, 1e-6, N_episodes)
+    end
     Q = zeros(length(mdp.S), length(mdp.A))
     i = 1
-    α = LinRange(1, 1e-6, N_episodes*T)
     for n in 1:N_episodes
         s = sampleInitialState(mdp)
         for t in 1:T
             a = sample(π, s, Q)
             s_new, r, done = step(mdp, s, a)
             Q[s,a] += α[i] * (r + mdp.γ * maximum(Q[s_new,a]) - Q[s,a])
-            i += 1
             s = s_new
             if done break end
         end
+        i += 1
     end
     return Q
 end
 
-function DoubleQLearning(π::GLIEPolicy, mdp::MDP, N_episodes::Number, T::Number)
+function DoubleQLearning(π::GLIEPolicy, mdp::MDP, N_episodes::Number, T::Number, α = nothing)
     reset!(π)
+    if α === nothing
+        α = LinRange(1, 1e-6, N_episodes)
+    end
     Q_1 = zeros(length(mdp.S), length(mdp.A))
     Q_2 = zeros(length(mdp.S), length(mdp.A))
     i = 1
-    α = LinRange(1, 1e-6, N_episodes*T)
     for n in 1:N_episodes
         s = sampleInitialState(mdp)
         for t in 1:T
@@ -34,10 +38,10 @@ function DoubleQLearning(π::GLIEPolicy, mdp::MDP, N_episodes::Number, T::Number
             else
                 Q_2[s,a] += α[i] * (r + mdp.γ * Q_1[s_new,argmax(Q_2[s_new,:])] - Q_2[s,a])
             end
-            i += 1
             s = s_new
             if done break end
         end
+        i += 1
     end
     return Q_1, Q_2
 end
