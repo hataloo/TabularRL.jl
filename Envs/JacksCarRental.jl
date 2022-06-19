f4382b1ba9d6301eff31a81a02f82f4713484056
hataloo <jacksandberg998@gmail.com>
2022-06-19 19:54:15 +0200

    Parallelized creating JacksCarRental

--- Envs/JacksCarRental.jl
+++ Envs/JacksCarRental.jl
@@ -1,14 +1,12 @@
 using Distributions, DataStructures
 
-function getJacksCarRentalMDP(γ = 0.9; λ_requests::Vector{Int64} = [3, 4], λ_returns::Vector{Int64} = [3, 2], carsMax::Int = 20, moveMax::Int = 5)
+function getJacksCarRentalMDP(γ = 0.9; λ_requests::Vector{Int64} = [3, 4], λ_returns::Vector{Int64} = [3, 2], carsMax::Int = 20, moveMax::Int = 5, verbose::Bool = false)
     @assert length(λ_requests) == length(λ_returns) "Length of requests and returns must be equal, given $(length(λ_requests)) and $(length(λ_returns))"
     @assert length(λ_requests) == 2 "Number of locations (requests and returns) must equal 2"
     numRentalLocations = length(λ_requests)
     S = Vector{Int64}(1:((carsMax+1) ^ numRentalLocations))
     A = Vector{Int64}(-moveMax:moveMax)
 
-    #P[s', r' | s, a]
-    stateRewardTransitionProbs = fill(0.0, repeat([carsMax+1], 3 * numRentalLocations)..., 2 *moveMax + 1)
     poissonpdf(x::Int64, λ::Real) = λ^x * exp(-λ)/factorial(x) 
     censoredpoissonpdf(x::Int64, λ::Real, max::Int64) = begin
         @assert x <= max "Censoredpoissonpdf does not accept arguments above max = $max, given x = $x, λ = $λ"
@@ -19,70 +17,77 @@ function getJacksCarRentalMDP(γ = 0.9; λ_requests::Vector{Int64} = [3, 4], λ_
             return poissonpdf(x, λ)
         end
     end
-
+    #P[s', r' | s, a]
+    stateRewardTransitionProbs = fill(0.0, repeat([carsMax+1], 2 * numRentalLocations)..., 2 *moveMax + 1, carsMax+1, carsMax+1)
+    
     t = @elapsed begin
-    for s_old1 = 0:carsMax, s_old2 = 0:carsMax
-        for s_new1 = 0:carsMax, s_new2 = 0:carsMax
+    @Threads.threads for s_new1 = 0:carsMax
+    @Threads.threads for s_new2 = 0:carsMax
+    for s_old1 = 0:carsMax 
+    for s_old2 = 0:carsMax
             for (i_a, a) in enumerate(A)
                 movedCars = clamp(a, -minimum([s_old1, carsMax - s_old2]), minimum([s_old2, carsMax - s_old1]))
                 stock1 = s_old1 + movedCars
                 stock2 = s_old2 - movedCars
-                # println("a = $a, movedCars = $movedCars, s_old1 = $s_old1, s_old2 = $s_old2")
-                # println("stock1 = $stock1, stock2 = $stock2, s_new1 = $s_new1, s_new2 = $s_new2")
-                #println("$s_old1, $s_old2, $stock1, $stock2, $a, $movedCars")
-                # reqDist1 = censored(Poisson(λ_requests[1]), 0, stock1)
-                # reqDist2 = censored(Poisson(λ_requests[2]), 0, stock2)
                 lower1 = maximum([-s_new1 + stock1, 0])
                 lower2 = maximum([-s_new2 + stock2, 0])
-                #println("$lower1, $lower2")
                 for req1 in lower1:stock1, req2 in lower2:stock2
                     ret1 = req1 + s_new1 - stock1
                     ret2 = req2 + s_new2 - stock2
-                    # retDist1 = censored(Poisson(λ_returns[1]), 0, carsMax - stock1 + req1)
-                    # retDist2 = censored(Poisson(λ_returns[2]), 0, carsMax - stock2 + req2)
-                    
-                    #println("req1 = $req1, req2 = $req2, ret1 = $ret1, ret2 = $ret2")
                     stateRewardTransitionProbs[
                         s_new1+1, s_new2+1, 
                         s_old1+1, s_old2+1,
-                        req1+1, req2+1, i_a] = 
-                        # pdf(reqDist1, req1) * pdf(reqDist2, req2) *
-                        # pdf(retDist1, ret1) * pdf(retDist2, ret2)
+                        i_a,
+                        req1+1, req2+1] = 
                         censoredpoissonpdf(req1, λ_requests[1], stock1) * 
                         censoredpoissonpdf(req2, λ_requests[2], stock2) * 
                         censoredpoissonpdf(ret1, λ_returns[1], carsMax - stock1 + req1) * 
                         censoredpoissonpdf(ret2, λ_returns[2], carsMax - stock2 + req2)
                 end
             end
-        end
+        end 
+    end
+    end
     end 
     end
-    println("time: $t")
-    println(maximum(abs.(sum(stateRewardTransitionProbs, dims = [1,2,5,6]) .- 1.0)))
+    if verbose
+        println("Time to build stateRewardTransitionProbs: $t")
+        println(maximum(abs.(sum(stateRewardTransitionProbs, dims = [1,2,6,7]) .- 1.0)))
+    end
     rewardDistributions = Array{DiscreteNonParametric, numRentalLocations * 2 + 1}(undef, repeat([carsMax + 1], 2 * numRentalLocations) ..., 2 * moveMax + 1)
     stateTransitionProbs = fill(0.0, repeat([carsMax+1], 2 * numRentalLocations)..., 2 *moveMax + 1)
-    for s_old1 = 0:carsMax, s_old2 = 0:carsMax
-        for s_new1 = 0:carsMax, s_new2 = 0:carsMax
+    t = @elapsed begin
+    @Threads.threads for s_new1 = 0:carsMax
+    @Threads.threads for s_new2 = 0:carsMax
+    for s_old1 = 0:carsMax
+    for s_old2 = 0:carsMax
             for (i_a, a) in enumerate(A)
                 movedCars = clamp(a, -minimum([s_old1, carsMax - s_old2]), minimum([s_old2, carsMax - s_old1]))
                 stock1 = s_old1 + movedCars
                 stock2 = s_old2 - movedCars
                 
-                stateTransProb = sum(stateRewardTransitionProbs[s_new1+1, s_new2+1, s_old1+1, s_old2+1, :, :, i_a])
+                stateTransProb = sum(stateRewardTransitionProbs[s_new1+1, s_new2+1, s_old1+1, s_old2+1, i_a, :, :])
                 stateTransitionProbs[s_new1+1, s_new2+1, s_old1+1, s_old2+1, i_a] = stateTransProb
 
                 outcomes = (-2*abs(a)):10:(10 * (stock1 + stock2) - 2 * abs(a))
                 probs = OrderedDict{Int64, Float64}(k => 0.0 for k in outcomes)
-                for req1 in 0:stock1, req2 in 0:stock2
+                lower1 = maximum([-s_new1 + stock1, 0])
+                lower2 = maximum([-s_new2 + stock2, 0])
+                for req1 in lower1:stock1, req2 in lower2:stock2
                     reward = 10 * (req1 + req2) - 2 * abs(a)
-                    probs[reward] = probs[reward] + stateRewardTransitionProbs[s_new1+1, s_new2+1, s_old1+1, s_old2+1, req1+1, req2+1, i_a] / stateTransProb
+                    probs[reward] = probs[reward] + stateRewardTransitionProbs[s_new1+1, s_new2+1, s_old1+1, s_old2+1, i_a, req1+1, req2+1, ] / stateTransProb
                 end
                 probVector = [v for v in values(probs)]
                 rewardDistributions[s_new1+1, s_new2+1, s_old1+1, s_old2+1, i_a] = DiscreteNonParametric(outcomes, probVector)
             end
-        end
+        end 
+    end
+    end
     end 
-
+    end
+    if verbose
+        println("Time to build reward & stateTransitionsProbs: $t")
+    end
     stateMap = Vector{Tuple{Int64, Int64}}(undef, length(S))
     count = 0
     for s_1 = 1:carsMax+1, s_2 = 1:carsMax+1
@@ -94,6 +99,6 @@ function getJacksCarRentalMDP(γ = 0.9; λ_requests::Vector{Int64} = [3, 4], λ_
     P = reshape(stateTransitionProbs, (carsMax + 1)^2, (carsMax + 1)^2, 2 * moveMax + 1)
     R = reshape(rewardDistributions, (carsMax + 1)^2, (carsMax + 1)^2, 2 * moveMax + 1)
     JacksCarRentalMDP = TabularMDP(P, R, μ, γ)
-    return P, JacksCarRentalMDP, stateMap
+    return JacksCarRentalMDP, stateMap
 
 end
--- runJacksCarRental.jl
+++ runJacksCarRental.jl
@@ -1,15 +1,18 @@
 include("TabularRL.jl")
 carsMax, moveMax = 12, 5
-P, mdp, stateMap = getJacksCarRentalMDP(carsMax = carsMax, moveMax = moveMax)
+t = @elapsed begin
+mdp, stateMap = getJacksCarRentalMDP(carsMax = carsMax, moveMax = moveMax)
+end
+println("timeOuter = $t")
 
-
-P_sum = sum(P, dims = [1])
+P_sum = sum(mdp.P, dims = [1])
 println("Maximum transition probability: $(maximum(P_sum))")
 println("Minimum transitition probability: $(minimum(P_sum))") 
 
 pi_opt, V_opt, P_opt = PolicyIteration(mdp, 100)
 
-reshape(V_opt, carsMax+1, carsMax+1)
-
-reshape([argmax(pi_opt[i,:])-(moveMax+1) for i in 1:size(pi_opt,1)], carsMax+1, carsMax+1)
+valueMatrix = reshape(V_opt, carsMax+1, carsMax+1)
+display(valueMatrix)
+optimalActionMatrix = reshape([argmax(pi_opt[i,:])-(moveMax+1) for i in 1:size(pi_opt,1)], carsMax+1, carsMax+1)
+display(optimalActionMatrix)
 
