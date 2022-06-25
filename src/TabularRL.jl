dec7c6df62cd4f5bf9960bb9f3b7a46ba382ef32
hataloo <jacksandberg998@gmail.com>
2022-06-25 13:26:19 +0200

    Moved files into src

--- src/TabularRL.jl
+++ src/TabularRL.jl
@@ -1,5 +1,62 @@
 module TabularRL
 
-# Write your package code here.
-
+include("MDP.jl")
+include("Policy.jl")
+include("Utils.jl")
+include("Algorithms/ValuePolicyIteration.jl")
+include("Algorithms/MonteCarlo.jl")
+include("Algorithms/TDLearning.jl")
+include("Algorithms/SARSA.jl")
+include("Algorithms/QLearning.jl")
+include("Envs/HallwayMDP.jl")
+include("Envs/GridWorlds.jl")
+include("Envs/CliffWalking.jl")
+include("Envs/JacksCarRental.jl")
+export 
+    #MDP.jl
+    TabularMDP, 
+    sampleInitialState,
+    sampleNextState,
+    sampleState,
+    step,
+    Episode,
+    sampleEpisode,
+    #Policy.jl
+    AbstractPolicy,
+    GLIEPolicy,
+    Policy,
+    getUniformPolicy,
+    sample,
+    EpsilonGreedyPolicy,
+    sample,
+    BoltzmannPolicy,
+    #Algorithms:
+        #ValuePolicyIteration
+        ValueIteration,
+        PolicyIteration,
+        ActionValueIteration,
+        #MonteCarlo
+        MonteCarlo,
+        #TDLearning
+        TD0,
+        TDnStep,
+        TDλ,
+        #SARSA
+        SARSA,
+        #QLearning,
+        QLearning,
+        DoubleQLearning,
+    #Envs:
+        #HallwayMDP.jl
+        getHallwayMDP,
+        #GridWorlds.jl
+        getMovementFunctions,
+        getWrappingMovementFunctions,
+        buildGridWalkTransitionProbabilities,
+        addTerminalState!,
+        addResettingState!,
+        #CliffWalking.jl
+        getCliffWalkingMDP,
+        #JacksCarRental.jl
+        getJacksCarRentalMDP
 end
